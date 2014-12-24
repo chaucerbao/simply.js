b1f4d929db6002ce2bc511af9d8bc75fad86f8f6
Alvin Chan <aschan@gmail.com>
2014-12-23 16:02:19 -0800

    Add a Tooltip example

--- examples/tooltip/script.js
+++ examples/tooltip/script.js
@@ -1,3 +1,8 @@
-simply.tooltip.bind(simply.select('*[data-tooltip]'), {
+simply.tooltip.bind(simply.select('*.section-1'), {
   class: 'style-1'
+});
+simply.tooltip.bind(simply.select('*.section-2'), {
+  class: 'style-2',
+  interactive: true,
+  position: 'top'
 });
